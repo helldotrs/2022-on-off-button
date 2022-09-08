a922998d50a703a6bc1eccaaba37b6a89bab50c4
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-09-08 09:38:41 +0200

    Update display.psudo.php

--- display.psudo.php
+++ display.psudo.php
@@ -1,8 +1,15 @@
 <?php
 
-$button_value = fetch_sql_boolean();
+require('sql/login-info.inc.php');
+require('sql/connect.inc.php');
+require('inc/sanitize.inc.php');
 
-if($button){
+$button_value = sanitize(fetch_sql_boolean()); //FIX ME
+
+//HTML:
+print('<!DOCTYPE html><html><head></head><body><pre><h1>');
+
+if($button_value){
   print("<p style="color:green">:)</p>");
 }else{
   print("<p style="color:red">:(</p>");
