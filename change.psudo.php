fb3a15884b64b79cccaaa3c23bc00633d174ce05
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-09-08 09:00:01 +0200

    Update change.psudo.php

--- change.psudo.php
+++ change.psudo.php
@@ -1,4 +1,25 @@
 <?php
+
+//print html
+print('
+  <form action="/action_page.php">
+    <input type="password" id="pass" name="pass">
+    <input type="submit">
+  </form>
+');
+
+//kill page if no input found
+if (!$_POST['pass']){
+  die();
+}
+
+//IMPORT CONNECT SQL HERE
+
+function sanitize(x){
+  x = real_escape_string(x);
+  return x;
+};
+
 $php_pass   = "password";
 $post_pass  = sanitize($_POST['pass']);
 
@@ -6,13 +27,6 @@ if($post_pass == $php_pass){
   setSqlValue(TRUE);
 }
 
-if($time == $midnight){
-  setSqlValue = FALSE;
-}
 
-print('
-  <form action="/action_page.php">
-    <input type="password" id="pass" name="pass">
-    <input type="submit">
-  </form>
-'); 
+
+ 
