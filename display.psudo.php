43994e7f99feab4731c4307c2ee424eba4a15ced
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-09-08 10:05:50 +0200

    Update display.psudo.php

--- display.psudo.php
+++ display.psudo.php
@@ -6,11 +6,19 @@ require('inc/sanitize.inc.php');
 
 $button_value = sanitize(fetch_sql_boolean()); //FIX ME
 
+//fetch value
+$sql = "SELECT only_value FROM hellmak-on-off";
+$result = $conn->query($sql);
+$button_value = sanitize($sql);
+$conn->close();
+
 //HTML:
-print('<!DOCTYPE html><html><head></head><body><pre><h1>');
+print('<!DOCTYPE html><html><head></head><body><pre><h1><p style="size:100px;');
 
 if($button_value){
-  print("<p style="color:green">:)</p>");
+  print('color:green">:)');
 }else{
-  print("<p style="color:red">:(</p>");
+  print('color:red">:(');
 }
+
+print('</p><pre></body></html>');
