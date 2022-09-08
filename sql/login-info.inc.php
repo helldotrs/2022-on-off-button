e14acb709cee2363db59a217f8d458f7bef9bef8
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-09-08 09:30:33 +0200

    Update login-info.inc.php

--- sql/login-info.inc.php
+++ sql/login-info.inc.php
@@ -4,3 +4,5 @@
   $sql_pass=  "W0RD_p455_";
   $sql_db=    "hellmak-on-off";
 echo "<!--include for sql info-->\n";
+
+//src:https://github.com/hellmak/2021_votes_bandnames/blob/main/inc/sql-login.inc.php
